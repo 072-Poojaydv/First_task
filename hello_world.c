51c294ba9d8786fd0bf11cb5b669b50f81088b7a
072-Poojaydv <77005793+072-Poojaydv@users.noreply.github.com>
2021-01-09 07:54:13 +0530

    Add files via upload
    First file in main!!

---
+++ hello_world.c
@@ -0,0 +1,14 @@
+/* hello.c
+   A first program in C */
+#include <stdio.h>
+#include <conio.h>
+#include <stdlib.h>
+
+void main()
+{
+   system("cls");
+   printf("Hello Pooja\n");
+    printf("My new git project in brach called developer");
+   printf("My First Program yeah!!!!!!!!\n");
+   getch();
+}
